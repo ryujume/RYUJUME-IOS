e5b0aa34d3f7337a387630070a997265f6a9658e
DAEUN28 <acone1128@gmail.com>
2019-07-04 12:27:30 +0900

    ADD MoreInformation.swift

---
+++ RyujumeApp/RyujumeApp/Util/MoreInfomation.swift
@@ -0,0 +1,18 @@
+
+//
+//  File.swift
+//  RyujumeApp
+//
+//  Created by baby1234 on 28/06/2019.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import Foundation
+
+enum MoreInfomation{
+    case career
+    case academicBackground
+    case prizeAndOthers
+    case foreignLanguage
+    case link
+}
