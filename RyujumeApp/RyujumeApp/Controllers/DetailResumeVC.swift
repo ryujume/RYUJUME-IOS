e6e8075bd0cbd7d93c98c7d51d8683d6abe2423c
DAEUN28 <acone1128@gmail.com>
2019-07-22 09:11:34 +0900

    Update DetailResumeVC.swift

--- RyujumeApp/RyujumeApp/Controllers/DetailResumeVC.swift
+++ RyujumeApp/RyujumeApp/Controllers/DetailResumeVC.swift
@@ -9,22 +9,25 @@
 import UIKit
 
 class DetailResumeVC: UIViewController {
-
+    
+    @IBOutlet weak var likeBtn: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var contentView: UIView!
+    
+    @IBOutlet weak var identityImgView: UIImageView!
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var phoneNumberLbl: UILabel!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var simpleIntroduceTxtView: UITextView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
