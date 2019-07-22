5d3dfa42a8de3377739b9a5916c9173859ac5bc5
DAEUN28 <acone1128@gmail.com>
2019-07-22 09:11:32 +0900

    Update MyResumeVC.swift

--- RyujumeApp/RyujumeApp/Controllers/MyResumeVC.swift
+++ RyujumeApp/RyujumeApp/Controllers/MyResumeVC.swift
@@ -8,6 +8,7 @@
 
 import UIKit
 import TextFieldEffects
+import SwiftyPickerPopover
 
 class MyResumeVC: UIViewController {
     
@@ -21,7 +22,7 @@ class MyResumeVC: UIViewController {
     @IBOutlet weak var identityImgView: UIImageView!
     @IBOutlet weak var phoneNumberTxtField: HoshiTextField!
     @IBOutlet weak var emailTxtField: HoshiTextField!
-    @IBOutlet weak var simpleIntroduceTxtView: roundTxtView!
+    @IBOutlet weak var simpleIntroduceTxtView: UITextView!
     
     @IBOutlet weak var careerView: UIView!
     @IBOutlet weak var academicBackgroundView: UIView!
@@ -53,26 +54,70 @@ class MyResumeVC: UIViewController {
     @IBAction func addCareerBtnAction(_ sender: UIButton) {
         viewArr.addCareerViewArray = Array<AddCareerView>()
         guard let newView = self.addView(stateOfButton: .career, stackView: careerStackView) as? AddCareerView else {return}
+        newView.startDateBtn.addTarget(self, action: #selector(tapedDateBtn), for: .touchUpInside)
+        newView.endDateBtn.addTarget(self, action: #selector(tapedDateBtn), for: .touchUpInside)
         viewArr.addCareerViewArray.append(newView)
     }
+    
     @IBAction func addAcademicBackgroundBtnAction(_ sender: UIButton) {
         viewArr.addAcademicBackgroundViewArray = Array<AddAcademicBackgroundView>()
         guard let newView = self.addView(stateOfButton: .academicBackground, stackView: academicBackgroundStackView) as? AddAcademicBackgroundView else {return}
+        newView.startDateBtn.addTarget(self, action: #selector(tapedDateBtn), for: .touchUpInside)
+        newView.endDateBtn.addTarget(self, action: #selector(tapedDateBtn), for: .touchUpInside)
         viewArr.addAcademicBackgroundViewArray.append(newView)
     }
+    
     @IBAction func addPrizeAndOthersBtnAction(_ sender: UIButton) {
         viewArr.addPrizeAndOthersViewArray = Array<AddPrizeAndOthersView>()
         guard let newView = self.addView(stateOfButton: .prizeAndOthers, stackView: prizeAndOthersStackView) as? AddPrizeAndOthersView else {return}
+        newView.prizedDateBtn.addTarget(self, action: #selector(tapedDateBtn), for: .touchUpInside)
         viewArr.addPrizeAndOthersViewArray.append(newView)
     }
+    
     @IBAction func addForeignLanguageBtnAction(_ sender: UIButton) {
         viewArr.addForeignLanguageViewArray = Array<AddForeignLanguageView>()
         guard let newView = self.addView(stateOfButton: .foreignLanguage, stackView: foreignLanguageStackView) as? AddForeignLanguageView else {return}
+        newView.levelBtn.addTarget(self, action: #selector(tapedDateBtn), for: .touchUpInside)
         viewArr.addForeignLanguageViewArray.append(newView)
     }
+    
     @IBAction func addLinkBtnAction(_ sender: Any) {
         viewArr.addLinkViewArray = Array<AddLinkView>()
         guard let newView = self.addView(stateOfButton: .link, stackView: linkStackView) as? AddLinkView else {return}
         viewArr.addLinkViewArray.append(newView)
+    }
+    
+    @objc func tapedDateBtn(sender: UIButton) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "YYYY.MM"
+        
+        if sender.tag == 0 {
+            DatePickerPopover(title: "시작일")
+                .setDateMode(.date)
+                .setSelectedDate(Date())
+                .setDoneButton { (popover, selectedDate) in
+                    sender.setTitle(formatter.string(from: selectedDate), for: .normal)}
+                .appear(originView: sender, baseViewController: self)
+        } else if sender.tag == 1{
+            DatePickerPopover(title: "마지막일")
+                .setDateMode(.date)
+                .setSelectedDate(Date())
+                .setDoneButton { (popover, selectedDate) in
+                    sender.setTitle(formatter.string(from: selectedDate), for: .normal)}
+                .appear(originView: sender, baseViewController: self)
+        } else if sender.tag == 2{
+            DatePickerPopover(title: "수상일")
+                .setDateMode(.date)
+                .setSelectedDate(Date())
+                .setDoneButton { (popover, selectedDate) in
+                    sender.setTitle(formatter.string(from: selectedDate), for: .normal)}
+                .appear(originView: sender, baseViewController: self)
+        } else {
+            StringPickerPopover(title: "수준", choices: ["유창함", "비즈니스회화", "일상회화"])
+                .setDoneButton { (popover, selectedRow, selectedString) in
+                    sender.setTitle(selectedString, for: .normal)
+                }
+                .appear(originView: sender, baseViewController: self)
+        }
     }
 }
