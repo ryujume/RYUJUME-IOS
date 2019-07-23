6fccb666ae297aaf9659223bb5e1cafab1dd2d0b
DAEUN28 <acone1128@gmail.com>
2019-07-23 11:12:21 +0900

    ADD Models

---
+++ RyujumeApp/RyujumeApp/Models/GetRyujumesModel.swift
@@ -0,0 +1,23 @@
+//
+//  GetRyujumes.swift
+//  RyujumeApp
+//
+//  Created by baby1234 on 22/07/2019.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import Foundation
+
+struct GetSimpleRyujumesModel: Codable {
+    let userName: String
+    let ryujumeId: String
+    let simpleIntroduce: String
+    let likeCount: Int
+    
+    enum CodingKeys: String, CodingKey {
+        case userName
+        case ryujumeId = "_id"
+        case simpleIntroduce = "simpleInfo"
+        case likeCount = "likeNumber"
+    }
+}
