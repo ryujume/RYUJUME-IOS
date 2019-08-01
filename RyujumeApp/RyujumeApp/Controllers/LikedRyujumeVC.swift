15f5210130b4b2b7c4761dbf0d6b40a9fe720430
DAEUN28 <acone1128@gmail.com>
2019-08-01 09:09:24 +0900

    ADD VC

---
+++ RyujumeApp/RyujumeApp/Controllers/LikedRyujumeVC.swift
@@ -0,0 +1,27 @@
+//
+//  LikeResumeVC.swift
+//  RyujumeApp
+//
+//  Created by baby1234 on 25/06/2019.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import UIKit
+
+class LikedRyujumeVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    
+}
+
+class LikedResumeCell: UICollectionViewCell {
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var selfIntroduceTxtView: UITextView!
+    
+}
