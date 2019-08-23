cf1ff58f1c6be5dc3e03bae93c87bc1597f30806
DAEUN28 <acone1128@gmail.com>
2019-08-24 05:46:07 +0900

    Delete All for resetting

--- RyujumeApp/Pods/Target Support Files/Pods-RyujumeApp/Pods-RyujumeApp-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RyujumeApp : NSObject
-@end
-@implementation PodsDummy_Pods_RyujumeApp
-@end
--- RyujumeApp/RyujumeApp/Views/MainModel.swift
+++
@@ -1,9 +0,0 @@
-//
-//  MainModel.swift
-//  memoapp
-//
-//  Created by baby1234 on 01/08/2019.
-//  Copyright © 2019 이현욱. All rights reserved.
-//
-
-import Foundation
