e914a56e9823dbbbf79f022576276000d7c2e2c5
DAEUN28 <acone1128@gmail.com>
2019-09-09 17:44:06 +0900

    Update RegisterVC.swift
    Connect to ViewModel

--- Ryujume-v1.0/RyujumeApp/RyujumeApp/ViewController/RegisterVC.swift
+++ Ryujume-v1.0/RyujumeApp/RyujumeApp/ViewController/RegisterVC.swift
@@ -8,6 +8,9 @@
 
 import UIKit
 
+import RxSwift
+import RxCocoa
+
 class RegisterVC: UIViewController {
 
     @IBOutlet weak var idTxtField: UITextField!
@@ -18,17 +21,40 @@ class RegisterVC: UIViewController {
     @IBOutlet weak var pwUnderlineView: UIView!
     @IBOutlet weak var nameUnderlineView: UIView!
     
+    @IBOutlet weak var doneBtn: UIButton!
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        bindViewModel()
     }
     
     override func viewWillAppear(_ animated: Bool) {
         self.navigationController?.navigationBar.isHidden = false
     }
     
-    @IBAction func doneBtnAction(_ sender: UIButton) {
+    private func bindViewModel() {
+        let input = RegisterVM.input(id: idTxtField.rx.text.orEmpty.asDriver(),
+                                     pw: pwTxtField.rx.text.orEmpty.asDriver(),
+                                     name: nameTxtField.rx.text.orEmpty.asDriver(),
+                                     registerTaps: doneBtn.rx.tap.asSignal())
+        let output = RegisterVM().transform(input: input)
         
+        output.registerInfoCheck.filter {$0 == false}
+            .drive(onNext: { [unowned self] _ in self.presentOKAlert(title: "회원가입 정보를 확인해주세요", message: nil) })
+            .disposed(by: disposeBag)
+        
+        output.registerResult
+            .filter { [unowned self] (result) -> Bool in
+                if result == .failure {
+                    self.presentOKAlert(title: "회원가입에 실패했습니다", message: nil)
+                    return false
+                }
+                return true
+            }.drive(onNext: { [unowned self] _ in
+                self.presentOKAlert(title: "회원가입 성공", message: "로그인화면으로 돌아가\n 로그인해주세요")
+            })
+            .disposed(by: disposeBag)
     }
     
 }
