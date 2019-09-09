e44f712c9240e1feda72af177fdb8cec41d4e2f7
DAEUN28 <acone1128@gmail.com>
2019-09-09 17:41:52 +0900

    Create NetworkingResult.swift

---
+++ Ryujume-v1.0/RyujumeApp/RyujumeApp/Util/NetworkingResult.swift
@@ -0,0 +1,14 @@
+//
+//  NetworkingResult.swift
+//  RyujumeApp
+//
+//  Created by baby1234 on 08/09/2019.
+//  Copyright © 2019 baby1234. All rights reserved.
+//
+
+import Foundation
+
+enum NetworkingResult {
+    case failure
+    case success
+}
