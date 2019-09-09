08a0da3e0555f8fe0926a61841c7e280897be750
DAEUN28 <acone1128@gmail.com>
2019-09-09 20:21:42 +0900

    Update LoginVM.swift
    Implementation completion

--- Ryujume-v1.0/RyujumeApp/RyujumeApp/ViewModel/LoginVM.swift
+++ Ryujume-v1.0/RyujumeApp/RyujumeApp/ViewModel/LoginVM.swift
@@ -7,3 +7,45 @@
 //
 
 import Foundation
+
+import RxSwift
+import RxCocoa
+
+final class LoginVM: ViewModelType {
+    struct Input {
+        let id: Driver<String>
+        let pw: Driver<String>
+        let loginTaps: Signal<Void>
+    }
+    
+    struct Output {
+        let result: Driver<NetworkingResult>
+        let infoCheck: Driver<Bool>
+    }
+    
+    func transform(input: LoginVM.Input) -> LoginVM.Output {
+        let info = Driver.combineLatest(input.id, input.pw) {($0,$1)}
+        
+        let infoCheck = input.loginTaps.asObservable().withLatestFrom(info)
+            .flatMapLatest { (id, pw) -> Observable<Bool> in
+                if id.count > 3 && pw.count > 3 {
+                    return Observable.just(true)
+                }
+                return Observable.just(false)
+            }.asDriver(onErrorJustReturn: false)
+        
+        let result = input.loginTaps.asObservable().withLatestFrom(info)
+            .flatMapLatest { (info) -> Observable<NetworkingResult> in
+                let (id,pw) = info
+                return AuthAPI().postLogin(id: id, pw: pw).map({ (loginModel) -> NetworkingResult in
+                    guard let loginModel = loginModel else {
+                    return .failure
+                    }
+                    Token.token = loginModel.token
+                    return .success
+                })
+            }.asDriver(onErrorJustReturn: .failure)
+        
+        return Output(result: result, infoCheck: infoCheck)
+    }
+}
